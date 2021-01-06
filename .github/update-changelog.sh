29578f409ba2404b9a52d2f7f563868adba1517b
Werner Dijkerman <werner@dj-wasabi.nl>
2021-01-06 17:02:54 +0100

    Reorder actionsg

--- .github/update-changelog.sh
+++ .github/update-changelog.sh
@@ -5,14 +5,12 @@ set -x
 # repository is the 'dj-wasabi-release' next to 'main'
 cd main
 
-# Generate markdown
+# Generate CHANGELOG.md file
 ../dj-wasabi-release/release.sh -d
 
-# Let commit the changes
 git status
 
-git rev-parse --abbrev-ref HEAD
-
+# Let commit the changes if there are any? (Well there should be!)
 if [[ $(git status | grep modified | wc -l) -ge 1 ]]
   then  echo "Committing file"
         git config --global user.name 'Werner Dijkerman [GH bot]'
@@ -20,5 +18,5 @@ if [[ $(git status | grep modified | wc -l) -ge 1 ]]
 
         git add CHANGELOG.md
         git commit -m "Updated CHANGELOG.md on \"$(date "+%Y-%m-%d %H:%M:%S")\"" CHANGELOG.md
-        # git push
+        git push
 fi
