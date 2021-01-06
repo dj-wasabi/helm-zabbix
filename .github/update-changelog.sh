9fb78cfbf81247b5bc1c4f1b59e009d98c0ecd9b
Werner Dijkerman <werner@dj-wasabi.nl>
2021-01-06 17:26:06 +0100

    Another attempt for debugging (2)

--- .github/update-changelog.sh
+++ .github/update-changelog.sh
@@ -8,16 +8,11 @@ cd main
 # Generate CHANGELOG.md file
 ../dj-wasabi-release/release.sh -d
 
-echo $CHANGELOG_GITHUB_TOKEN | sed -e 's/^\(.\{6\}\).*/\1/'
 
-git status
-pwd
-ls -l
-
-cat CHANGELOG.md
+git config --get remote.origin.url
 
 # Let commit the changes if there are any? (Well there should be!)
-if [[ $(git status | grep modified | wc -l) -ge 1 ]]
+if [[ $(git status | grep -c 'CHANGELOG.md' || true) -gt 0 ]]
   then  echo "Committing file"
         git config --global user.name 'Werner Dijkerman [GH bot]'
         git config --global user.email 'github@dj-wasabi.nl'
